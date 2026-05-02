98d82e9cb3c4138dd9920d94e9ba70dd162a4e73
omavashia <omavashia1233@gmail.com>
2026-05-01 17:57:04 -0700

    minor changes

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -9,7 +9,7 @@ return {
         ["<Down>"] = { "select_next", "fallback" },
         ["<CR>"] = { "accept", "fallback" },
       },
-      appearance = { use_nvim_highlights = true },
+      -- appearance = { use_nvim_highlights = true },
       sources = { default = { "lsp", "path", "snippets", "buffer" } },
     },
   },
--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -23,7 +23,7 @@ return {
       end,  
 	theme = "dragon",              -- Load "wave" theme 
 	background = {               -- map the value of 'background' option to a theme  
-        dark = "dragon",           -- try "dragon" !  	
+	 dark = "dragon",           -- try "dragon" !  	
       },  
     })  
   
